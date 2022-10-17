fc498353855efb89396e5cc4d16e0ed5ceed8f7d
Artem <mrdmitrievv@yandex.ru>
2022-10-17 18:18:28 +0300

    Change fetchImage function: replace URLSession with  DispatchWorkItem

--- AllPatternsApp/Services/ImageManager.swift
+++ AllPatternsApp/Services/ImageManager.swift
@@ -16,10 +16,23 @@ class ImageManager {
     func fetchImage(from url: URL?, completion: @escaping (Data?) -> Void) {
         guard let url = url else { return completion(nil) }
         
+        // MARK: - WorkItem, downloading works slower than URLSession
+        var data: Data?
+        let queue = DispatchQueue.global(qos: .utility)
+        let workItem = DispatchWorkItem(qos: .utility) {
+            data = try? Data(contentsOf: url)
+            completion(data)
+        }
+        
+        queue.async(execute: workItem)
+        
+        /*
         // MARK: - URLSession
+         
         URLSession.shared.dataTask(with: url) { data, _, _ in
             guard let imageData = data else { return }
             completion(imageData)
         }.resume()
+        */
     }
 }
